d8b8948518fe9e5b5e4b44d7b66232a68ea1826c
shohi <oshohi@gmail.com>
2017-10-27 18:02:19 +0800

    add github repos search

--- basic/github.go
+++ basic/github.go
@@ -2,18 +2,23 @@ package basic
 
 import (
 	"fmt"
+	"strings"
 
 	"github.com/asciimoo/colly"
 )
 
-// InsightBook fetch book data from douban and conclude some insights
-func InsightBook() {
+// InsightGithub - fetch github repos' stared and forking data
+// and conclude some insights
 
+var searchBaseURL = "https://github.com/search?q="
+var sep, collapse = ":", "+"
+
+func InsightGithub() {
 	// Instantiate default collector
 	c := colly.NewCollector()
 
 	// Visit only domains: douban.com, book.douban.com
-	c.AllowedDomains = []string{"douban.com", "book.douban.com"}
+	c.AllowedDomains = []string{"github.com"}
 
 	// On every a element which has href attribute call callback
 	c.OnHTML("a[href]", func(e *colly.HTMLElement) {
@@ -30,6 +35,20 @@ func InsightBook() {
 		fmt.Println("Visiting", r.URL.String())
 	})
 
-	// Start scraping on https://book.douban.com
-	c.Visit("https://book.douban.com")
+	// Start scraping on github.com
+	m := map[string]string{
+		"language": "go",
+		"stars":    ">500",
+	}
+	c.Visit(searchBaseURL + joinMap(m))
+
+}
+
+func joinMap(m map[string]string) string {
+	var entries []string
+	for k, v := range m {
+		entries = append(entries, k+sep+v)
+	}
+
+	return strings.Join(entries, collapse)
 }
---
+++ basic/github_test.go
@@ -0,0 +1,22 @@
+package basic
+
+import (
+	"testing"
+)
+
+func TestJoinMap(t *testing.T) {
+
+	cases := []struct {
+		input map[string]string
+		want  string
+	}{
+		{map[string]string{"key": "value"}, "key:value"},
+	}
+
+	for _, c := range cases {
+		got := joinMap(c.input)
+		if got != c.want {
+			t.Errorf("joinMap(%q) == %v, want %v", c.input, got, c.want)
+		}
+	}
+}
